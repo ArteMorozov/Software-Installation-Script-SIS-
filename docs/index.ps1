01a41789e22a0bf6cfbee55fc9cd5e98aebcd78c
ArteMorozov <163231286+ArteMorozov@users.noreply.github.com>
2026-08-12 16:04:19 +0500

    Delete docs directory

--- docs/index.ps1
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="refresh" content="0; url=https://raw.githubusercontent.com/ArteMorozov/Software-Installation-Script-SIS-/main/SIS.ps1">
-</head>
-<body>
-    <p>Перенаправление на SIS...</p>
-</body>
-</html>
